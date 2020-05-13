e9499f9ed586e9b0ddf120c3f765fb44a2634e02
Chinmay2307 <53855724+Chinmay2307@users.noreply.github.com>
2020-05-13 20:11:39 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,26 +1,132 @@
 import json
 import urllib.request, urllib.parse, urllib.error
 import xlsxwriter
+import os.path
 
 url = "https://api.covid19india.org/data.json"
+url2 = "https://api.covid19india.org/v2/state_district_wise.json"
+url3 = "https://api.covid19india.org/zones.json"
 uh = urllib.request.urlopen(url)
 data = uh.read().decode()
+uh2 = urllib.request.urlopen(url2)
+data2 = uh2.read().decode()
+uh3 = urllib.request.urlopen(url3)
+data3 = uh3.read().decode()
+js3 = json.loads(data3)
 print("Data Retrived")
 js = json.loads(data)
+js2 = json.loads(data2)
 tdate = js["cases_time_series"]
 state_data = js["statewise"]
 tested_data = js["tested"]
 
-check = 0
-row_worksheet1=1
-col_worksheet1=0        
+row_worksheet1,col_worksheet1,row_worksheet2,col_worksheet2,row_worksheet3,col_worksheet3,row_worksheet4,col_worksheet4 = 1,0,1,0,1,0,1,0
 
-row_worksheet2=1
-col_worksheet2=0        
+def createdatabase():
+    workbook = xlsxwriter.Workbook('Corona_DataBase.xlsx')
+    worksheet1 = workbook.add_worksheet('Case Time Series')
+    worksheet2 = workbook.add_worksheet('State Wise Cases')
+    worksheet3 = workbook.add_worksheet('Tests')
+    worksheet4 = workbook.add_worksheet('District Wise')
+    bold = workbook.add_format({'bold': True})
+    Red = workbook.add_format({'bg_color': '#FFC7CE'})
+    Orange = workbook.add_format({'bg_color': '#FFEB9C'})
+    Green = workbook.add_format({ 'bg_color': '#C6EFCE'})
+    num_type = workbook.add_format({'num_format': '0'})
 
-row_worksheet3=1
-col_worksheet3=0
+    worksheet1.write('A1', 'Date', bold)
+    worksheet1.write('B1', 'Daily Confirmed', bold)
+    worksheet1.write('C1', 'Dail Deceaced', bold)
+    worksheet1.write('D1', 'Daily Recovered', bold)
+    worksheet1.write('E1', 'Total Confirmed', bold)
+    worksheet1.write('F1', 'Total Deceased', bold)
+    worksheet1.write('G1', 'Total Recovered', bold)
+    worksheet1.set_column('A:A', 15)
+    worksheet1.set_column('B:G', 18)
 
+    worksheet2.write('A1', 'Last Updated Time', bold)
+    worksheet2.write('B1', 'State', bold)
+    worksheet2.write('C1', 'State Code', bold)
+    worksheet2.write('D1', 'Active', bold)
+    worksheet2.write('E1', 'Confirmed', bold)
+    worksheet2.write('F1', 'Deceaced', bold)
+    worksheet2.write('G1', 'Recovered', bold)
+    worksheet2.write('H1', 'Delta Confirmed', bold)
+    worksheet2.write('I1', 'Delta Deceased', bold)
+    worksheet2.write('J1', 'Delta Recovered', bold)
+    worksheet2.set_column('A:A', 25)
+    worksheet2.set_column('B:J', 18)
+
+    worksheet3.write('A1', 'Update Time Stamp', bold)
+    worksheet3.write('B1', 'Individuals tested per confirmed cases', bold)
+    worksheet3.write('C1', 'Positive cases from samples reported', bold)
+    worksheet3.write('D1', 'Sample reported today', bold)
+    worksheet3.write('E1', 'Test positivity rate', bold)
+    worksheet3.write('F1', 'Test conducted by private labs', bold)
+    worksheet3.write('G1', 'Tests per Confirmed cases', bold)
+    worksheet3.write('H1', 'Total individuals tested', bold)
+    worksheet3.write('I1', 'Total positive cases', bold)
+    worksheet3.write('J1', 'Total sample tested', bold)
+    worksheet3.set_column('A:J', 30)
+            
+    worksheet4.write('A1','State',bold)
+    worksheet4.write('B1','District',bold)
+    worksheet4.write('C1','Confirmed',bold)
+    worksheet4.write('D1','Active',bold)
+    worksheet4.write('E1','Recovered',bold)
+    worksheet4.write('F1','Deceased',bold)
+    worksheet4.set_column('A:F',30)
+
+    global row_worksheet1,col_worksheet1,row_worksheet2,col_worksheet2,row_worksheet3,col_worksheet3,row_worksheet4,col_worksheet4    
+
+    std = ["lastupdatedtime","state","statecode","active","confirmed","deaths","recovered","deltaconfirmed","deltadeaths","deltarecovered"]
+    for i in range(len(state_data)):
+        for k in range(10):
+            worksheet2.write(row_worksheet2, col_worksheet2 + k, js["statewise"][i][std[k]])
+        row_worksheet2 = row_worksheet2 + 1
+        col_worksheet2 = 0
+    dist = []
+    distdata = ["district","confirmed","active","recovered","deceased"]
+    zonesss = ["Red","Orange","Green"]
+    for m in range(735):
+        dist.append(js3["zones"][m]["district"])
+    for i in range(36):
+        state = js2[i]["state"]
+        for j in range(len(js2[i]["districtData"])):
+            for k in range(5):
+                try:
+                    y = dist.index(js2[i]["districtData"][j]["district"])
+                    x = zonesss.index(js3["zones"][y]["zone"])
+                    if x == 0:
+                        worksheet4.write(row_worksheet4, col_worksheet4,state,Red)
+                        worksheet4.write(row_worksheet4, col_worksheet4 + k +1,js2[i]["districtData"][j][distdata[k]],Red)
+                    elif x == 1:
+                        worksheet4.write(row_worksheet4, col_worksheet4,state,Orange)
+                        worksheet4.write(row_worksheet4, col_worksheet4 + k +1,js2[i]["districtData"][j][distdata[k]],Orange)
+                    elif x == 2:
+                        worksheet4.write(row_worksheet4, col_worksheet4,state,Green)
+                        worksheet4.write(row_worksheet4, col_worksheet4 + k +1,js2[i]["districtData"][j][distdata[k]],Green)
+                except:
+                    worksheet4.write(row_worksheet4, col_worksheet4,state)
+                    worksheet4.write(row_worksheet4, col_worksheet4 + k +1,js2[i]["districtData"][j][distdata[k]],)
+            row_worksheet4 = row_worksheet4 + 1
+
+            
+    cts = ["date","dailyconfirmed","dailydeceased","dailyrecovered","totalconfirmed","totaldeceased","totalrecovered"]
+    for i in range(len(tdate)):
+        for k in range(7):
+            worksheet1.write(row_worksheet1, col_worksheet1 + k, js["cases_time_series"][i][cts[k]])
+        row_worksheet1 = row_worksheet1 + 1
+                
+            
+    test = ["updatetimestamp","individualstestedperconfirmedcase","positivecasesfromsamplesreported","samplereportedtoday",
+            "testpositivityrate","testsconductedbyprivatelabs","testsperconfirmedcase","totalindividualstested","totalpositivecases","totalsamplestested"]
+    for i in range(len(tested_data)):
+        for k in range(10):
+            worksheet3.write(row_worksheet3, col_worksheet3 + k, js["tested"][i][test[k]])
+        row_worksheet3 = row_worksheet3 + 1
+
+    workbook.close()
 print("1.Total Cases")
 print("2.Total Recovered")
 print("3.Total Active Cases")
@@ -36,25 +142,25 @@ while True:
 
     if op == "1":
         y = js["statewise"][0]["confirmed"]
-        print("Total Cases >>",y)
+        print("Total Cases >>", y)
     elif op == "2":
         m = js["statewise"][0]["recovered"]
-        print("Total Recovered >>",m)
+        print("Total Recovered >>", m)
     elif op == "3":
         k = js["statewise"][0]["active"]
-        print("Total Active >>",k)
+        print("Total Active >>", k)
     elif op == "4":
-       l = js["statewise"][0]["deaths"]
-       print("Total Deaths >>",l)
+        l = js["statewise"][0]["deaths"]
+        print("Total Deaths >>", l)
     elif op == "5":
         confirm = js["statewise"][0]["confirmed"]
         re = js["statewise"][0]["recovered"]
         acti = js["statewise"][0]["active"]
         dead = js["statewise"][0]["deaths"]
-        print("Total Confirmed >>",confirm)
-        print("Total Recovered >>",re)
-        print("Total Active >>",acti)
-        print("Total Dead >>",dead)
+        print("Total Confirmed >>", confirm)
+        print("Total Recovered >>", re)
+        print("Total Active >>", acti)
+        print("Total Dead >>", dead)
 
     elif op == "6":
         name = input("Enter the name of the state code>>")
@@ -80,7 +186,7 @@ while True:
         print("New Confirmed >>", confirm)
         print("New Recovered >>", re)
         print("New Deaths >>", dead)
-        print("Last updated at>>",lastup)
+        print("Last updated at>>", lastup)
         print("........................................")
         print("........................................")
     elif op == "8":
@@ -105,116 +211,16 @@ while True:
                 print("........................................")
                 print("........................................")
     elif op == "9":
-        
-        if check == 0:
-            workbook=xlsxwriter.Workbook('Corona_DataBase.xlsx')
-            worksheet1=workbook.add_worksheet('Case Time Series')
-            worksheet2=workbook.add_worksheet('State Wise Cases')
-            worksheet3=workbook.add_worksheet('Tests')
-            bold=workbook.add_format({'bold' : True })
-            num_type=workbook.add_format({'num_format': '0'})
-            
 
-            worksheet1.write('A1','Date',bold)
-            worksheet1.write('B1','Daily Confirmed',bold)
-            worksheet1.write('C1','Dail Deceaced',bold)
-            worksheet1.write('D1','Daily Recovered',bold)
-            worksheet1.write('E1','Total Confirmed',bold)
-            worksheet1.write('F1','Total Deceased',bold)
-            worksheet1.write('G1','Total Recovered',bold)
-            worksheet1.set_column('A:A',15)
-            worksheet1.set_column('B:G',18)
-            
-            worksheet2.write('A1','Last Updated Time',bold)
-            worksheet2.write('B1','State',bold)
-            worksheet2.write('C1','State Code',bold)
-            worksheet2.write('D1','Active',bold)
-            worksheet2.write('E1','Confirmed',bold)
-            worksheet2.write('F1','Deceaced',bold)
-            worksheet2.write('G1','Recovered',bold)
-            worksheet2.write('H1','Delta Confirmed',bold)
-            worksheet2.write('I1','Delta Deceased',bold)
-            worksheet2.write('J1','Delta Recovered',bold)
-            worksheet2.set_column('A:A',25)
-            worksheet2.set_column('B:J',18)
-
-            worksheet3.write('A1','Update Time Stamp',bold)
-            worksheet3.write('B1','Individuals tested per confirmed cases',bold)
-            worksheet3.write('C1','Positive cases from samples reported',bold)
-            worksheet3.write('D1','Sample reported today',bold)
-            worksheet3.write('E1','Test positivity rate',bold)
-            worksheet3.write('F1','Test conducted by private labs',bold)
-            worksheet3.write('G1','Tests per Confirmed cases',bold)
-            worksheet3.write('H1','Total individuals tested',bold)
-            worksheet3.write('I1','Total positive cases',bold)
-            worksheet3.write('J1','Total sample tested',bold)
-            worksheet3.set_column('A:A',25)
-            worksheet3.set_column('B:B',35)
-            worksheet3.set_column('C:C',35)
-            worksheet3.set_column('D:D',25)
-            worksheet3.set_column('E:E',23)
-            worksheet3.set_column('F:F',30)
-            worksheet3.set_column('G:G',23)
-            worksheet3.set_column('H:H',23)
-            worksheet3.set_column('I:I',22)
-            worksheet3.set_column('J:J',22)
-
-            for i in range(len(state_data)):
-                
-                worksheet2.write(row_worksheet2,col_worksheet2,js["statewise"][i]["lastupdatedtime"])
-                worksheet2.write(row_worksheet2,col_worksheet2+1,js["statewise"][i]["state"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+2,js["statewise"][i]["statecode"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+3,js["statewise"][i]["active"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+4,js["statewise"][i]["confirmed"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+5,js["statewise"][i]["deaths"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+6,js["statewise"][i]["recovered"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+7,js["statewise"][i]["deltaconfirmed"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+8,js["statewise"][i]["deltadeaths"],num_type)
-                worksheet2.write(row_worksheet2,col_worksheet2+9,js["statewise"][i]["deltarecovered"],num_type)
-
-
-                row_worksheet2=row_worksheet2+1
-                col_worksheet2=0
-
-            
-
-            for i in range(len(tdate)):
-                
-                worksheet1.write(row_worksheet1,col_worksheet1,js["cases_time_series"][i]["date"])
-                worksheet1.write(row_worksheet1,col_worksheet1+1,js["cases_time_series"][i]["dailyconfirmed"],num_type)
-                worksheet1.write(row_worksheet1,col_worksheet1+2,js["cases_time_series"][i]["dailydeceased"],num_type)
-                worksheet1.write(row_worksheet1,col_worksheet1+3,js["cases_time_series"][i]["dailyrecovered"],num_type)
-                worksheet1.write(row_worksheet1,col_worksheet1+4,js["cases_time_series"][i]["totalconfirmed"],num_type)
-                worksheet1.write(row_worksheet1,col_worksheet1+5,js["cases_time_series"][i]["totaldeceased"],num_type)
-                worksheet1.write(row_worksheet1,col_worksheet1+6,js["cases_time_series"][i]["totalrecovered"],num_type)
-                
-                row_worksheet1=row_worksheet1+1
-                col_worksheet1=0
-
-
-
-            for i in range(len(tested_data)):
-                
-                worksheet3.write(row_worksheet3,col_worksheet3,js["tested"][i]["updatetimestamp"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+1,js["tested"][i]["individualstestedperconfirmedcase"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+2,js["tested"][i]["positivecasesfromsamplesreported"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+3,js["tested"][i]["samplereportedtoday"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+4,js["tested"][i]["testpositivityrate"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+5,js["tested"][i]["testsconductedbyprivatelabs"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+6,js["tested"][i]["testsperconfirmedcase"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+7,js["tested"][i]["totalindividualstested"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+8,js["tested"][i]["totalpositivecases"],num_type)
-                worksheet3.write(row_worksheet3,col_worksheet3+9,js["tested"][i]["totalsamplestested"],num_type)
-                 
-
-                row_worksheet3=row_worksheet3+1
-                col_worksheet3=0
-
-            workbook.close()
-            check=1
-            print("DataBase Created")
+        if os.path.exists('Corona_DataBase.xlsx') == True:
+            print("DataBase already exists")
+            check = input("Do you want to update Database")
+            if check == "yes":
+                createdatabase()
+                print("Database updated")
         else:
-            print("DataBase is already created")
+            createdatabase()
+            print("Database Created")
     elif op == "10":
         print("Thank You!!")
         break
